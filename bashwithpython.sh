5965ced14f6424c57b1a9e6177303994f47f2d9b
imjoseangel <josea.munoz@gmail.com>
2015-09-01 11:26:15 +0200

    Create bashwithpython.sh

---
+++ bashwithpython.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function get_data {
+
+python - <<END
+
+import urllib2
+import urllib
+
+url = 'https://10.100.20.94/Sourcing/inspector'
+username = 'debug'
+password = 'password'
+
+p = urllib2.HTTPPasswordMgrWithDefaultRealm()
+
+p.add_password(None, url, username, password)
+
+handler = urllib2.HTTPBasicAuthHandler(p)
+opener = urllib2.build_opener(handler)
+urllib2.install_opener(opener)
+
+urlaql = 'https://10.100.20.94/Sourcing/inspector/aql'
+
+post_data = {'restrictQueryBy': 'None', 'query': 'SELECT Name, Dataloads.NodeName, Dataloads.State FROM ariba.analytics.server.DataloadSchedule include inactive ORDER BY Dataloads.TimeStarted desc,Dataloads.TimeScheduled desc', 'partitionNames': 'None', 'acrossCommunities': 'off', 'baseDomainVariant': 'off', 'joinType': 3, 'maxRecords': 500, 'userLocale': 'English', 'userPartition': 'Any', 'submit': 'Submit'}
+
+headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'POST': '/Sourcing/inspector/aql HTTP/1.1', 'Host': '10.100.20.94', 'Connection': 'keep-alive'}
+
+data = urllib.urlencode(post_data)
+req = urllib2.Request(urlaql, data, headers)
+response = urllib2.urlopen(req)
+query = response.read()
+
+print query
+
+END
+}
+
+get_data | html2text -width 100
