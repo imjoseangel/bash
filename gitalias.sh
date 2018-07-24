8388a2887faa1309e615a06efbb336f02c8ede14
Jose Angel <imjoseangel@users.noreply.github.com>
2018-07-24 08:38:14 +0200

    Update gitalias.sh

--- gitalias.sh
+++ gitalias.sh
@@ -4,6 +4,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st "status -sb"
+git config --global alias.last 'log -1 HEAD'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.lol "log --graph --decorate --oneline"
 git config --global alias.lola "log --graph --decorate --oneline --all"
